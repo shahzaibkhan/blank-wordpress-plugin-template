cbf21ab0394af6c32d213a6ca5541d54e3a7dc13
Shahzaib Khan <shahzaib198@gmail.com>
2017-03-02 22:12:48 +0500

    Updates to the Hello World Function.

--- defaultname/defaultname.php
+++ defaultname/defaultname.php
@@ -11,18 +11,14 @@ function theme_options_panel(){
   /* 	
 		Adding Menu to Main WordPress Side Menu 
 		add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null )
-
   */
   
   add_menu_page('Default Name', 'Default Name', 'manage_options', 'default-options', 'initialize_defaulplugin',plugins_url('/defaultname/img/icon.png',__DIR__));
 
   /* 
-	
 		Adding Sub Menu to your Menu 
 		add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '' )
-  
   */
- 
   add_submenu_page( 'default-options', 'About', 'About', 'manage_options', 'default-about', 'default_about');
   add_submenu_page( 'default-options', 'FAQ', 'FAQ', 'manage_options', 'default-faq', 'default_faq');
   
@@ -30,7 +26,7 @@ function theme_options_panel(){
 add_action('admin_menu', 'theme_options_panel');
 
 function initialize_defaulplugin(){
-	echo "Hello World!";
+	echo "Hello World";
 }
 function default_about(){ 
 	echo "About Page Here";
